7a50d7e7ba2a3a16c40fed8a42e4dfc1e8eb6c4b
Wahidullah Rahimi <wahidrahimi45@gmail>
2023-01-05 20:42:48 -0800

    User input and random number generation

--- OOPsInJava/src/Main.java
+++ OOPsInJava/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+/*        System.out.print("Please Enter Your Name: ");
+        String name = scanner.next();
+        System.out.printf("Please Enter Your Age: ");
+        int age = scanner.nextInt();
+
+        System.out.printf("Hello "+ name + " You are "+ age + " Years Old.");*/
+
+
+        // Generate Rando number in Java
+
+        Random random = new Random();
+
+        int num = random.nextInt(20); // 0 - 19
+        System.out.printf("the random number is: "+ num);
     }
+
 }
