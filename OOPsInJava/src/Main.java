51f1edc7a68a4e44db26dac3713248e02c5a3402
Wahidullah Rahimi <wahidrahimi45@gmail>
2023-01-05 21:21:32 -0800

    Equality concepts in String

--- OOPsInJava/src/Main.java
+++ OOPsInJava/src/Main.java
@@ -17,7 +17,52 @@ public class Main {
         Random random = new Random();
 
         int num = random.nextInt(20); // 0 - 19
-        System.out.printf("the random number is: "+ num);
+        System.out.println("the random number is: "+ num);
+
+        String[] name = {"Wahid"};
+        String str1 = "Wahid";
+        String str2 = "Wahid";
+        String str3 = new String("Wahid");
+        if(str1 ==  str2)
+        {
+            System.out.println("str1 and stt2 are equal");
+        }
+        if(str1 ==  name[0])
+        {
+            System.out.println("str1 and name[0] are equal");
+        }
+        if(str1 ==  str3)
+        {
+            System.out.println("str1 and str3 are equal");
+        }
+//        System.out.println(str1.hashCode());
+//        System.out.println(str2.hashCode());
+//        System.out.println(name[0].hashCode());
+//        System.out.println(str3.hashCode());
+
+        String input = scanner.next();
+        if(str1 == input) // when we use == to check 2 string we check the equality of the reference they hold, objects reference inside the RAM
+        {
+            System.out.println("str1 is equal to input");
+        }
+        //System.out.println(input.hashCode());
+
+        System.out.println();
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+        System.out.println(System.identityHashCode(name[0]));
+        System.out.println(System.identityHashCode(str3));
+        System.out.println(System.identityHashCode(input));
+
+        //Therefore, it is not a good idea to compare 2 strings using the ==
+        // instead we can use the equals method
+
+        if(str1.equals(input) && str1.equals(str2))
+        {
+            System.out.println("They are equal");
+        }
+
+
     }
 
 }
